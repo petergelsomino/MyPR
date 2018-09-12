53ea459d83561aff466e175daaff5412f2c0a7a3
petergelsomino <peter.gelsomino@gc.com>
2018-09-11 20:41:25 -0400

    setup table view for lift history

---
+++ MyPR/LiftHistoryTableView.swift
@@ -0,0 +1,21 @@
+//
+//  LiftHistoryTableView.swift
+//  MyPR
+//
+//  Created by Peter Gelsomino on 9/11/18.
+//  Copyright © 2018 PeteGels. All rights reserved.
+//
+
+import UIKit
+
+class LiftHistoryTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MyPR/LiftHistoryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LiftHistoryViewController.swift
+//  MyPR
+//
+//  Created by Peter Gelsomino on 9/11/18.
+//  Copyright © 2018 PeteGels. All rights reserved.
+//
+
+import UIKit
+
+class LiftHistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
