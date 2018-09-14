f24d4ea5f7eee607aebf2eb0b505bf81c2c79e4e
petergelsomino <peter.gelsomino@gc.com>
2018-09-14 17:27:41 -0400

    hooked up labels to 1 rep

--- MyPR/PercentagesViewController.swift
+++ MyPR/PercentagesViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class PercentagesViewController: UIViewController {
 
+    @IBOutlet weak var oneRepMax: UILabel!
+    @IBOutlet weak var twoRepMax: UILabel!
+    @IBOutlet weak var threeRepMax: UILabel!
+    @IBOutlet weak var fiveRepMax: UILabel!
+    
+    @IBOutlet weak var seventyPercentLabel: UILabel!
+    @IBOutlet weak var seventyFivePercentageLabel: UILabel!
+    @IBOutlet weak var eightyPercentageLabel: UILabel!
+    @IBOutlet weak var eightyFivePercentageLabel: UILabel!
+    @IBOutlet weak var nintyPercentageLabel: UILabel!
+    @IBOutlet weak var nintyFivePercentageLabel: UILabel!
+    @IBOutlet weak var oneHundredPercentageLabel: UILabel!
+    @IBOutlet weak var oneHundredFivePercentageLabel: UILabel!
+    
+    var repMaxFloat:Float = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        repMaxFloat = Float(oneRepMax.text!)!
+        calculatePercentagesBasedOnReps(repMax: repMaxFloat)
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +38,15 @@ class PercentagesViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func calculatePercentagesBasedOnReps(repMax: Float) {
+        seventyPercentLabel.text = String(Int((repMax * 0.7).rounded()))
+        seventyFivePercentageLabel.text = String(Int((repMax * 0.75).rounded()))
+        eightyPercentageLabel.text = String(Int((repMax * 0.80).rounded()))
+        eightyFivePercentageLabel.text = String(Int((repMax * 0.85).rounded()))
+        nintyPercentageLabel.text = String(Int((repMax * 0.9).rounded()))
+        nintyFivePercentageLabel.text = String(Int((repMax * 0.95).rounded()))
+        oneHundredPercentageLabel.text = String(Int((repMax).rounded()))
+        oneHundredFivePercentageLabel.text = String(Int((repMax * 1.05).rounded()))
     }
-    */
 
 }
